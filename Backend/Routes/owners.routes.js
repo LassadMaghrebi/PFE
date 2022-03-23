7bbdf35f428ad8473841a1a9c35b825ead87f52a
lassad <lassedmaghrebi@gmail.com>
2022-03-23 15:59:50 +0100

    first commit

--- Backend/Routes/owners.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router=express.Router();
-const ownerAuth = require("../middlewares/ownerAuth");
-const stadiumController = require("../controllers/stadiumController");
-const authController = require("../controllers/authController");
-router.post('/add',ownerAuth,stadiumController.AddStadium)
-router.post('/addPhoneNumber',authController.addPhoneNumber)
-router.post('/disableStadium',ownerAuth,stadiumController.disableStadium)
-module.exports = router
--- Backend/Routes/reservation.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Auth = require("../middlewares/auth");
-const ReservationController = require("../controllers/reservationController");
-router.post('/add',ReservationController.AddReservation)
-router.post('/find/all',ReservationController.getAllReservations)
-module.exports = router
